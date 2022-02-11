d2816933f7a2c2100d3dbf45b17b34972566f860
pauline rouger <paulinerouge@hotmail.com>
2022-02-11 16:14:36 +0800

    clean code

--- accordion/accordion.js
+++ accordion/accordion.js
@@ -1,10 +1,10 @@
+// Collapsible function by adding class "open"
+
 var acc = document.getElementsByClassName("item");
 var i;
 
 for (i = 0; i < acc.length; i++) {
   acc[i].addEventListener("click", function() {
-    /* Toggle between adding and removing the "open" class,
-    to highlight the button that controls the panel */
     this.classList.toggle("open");
   });
 }
